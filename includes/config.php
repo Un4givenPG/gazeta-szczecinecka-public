3a33866770c72c0417201882e69d1596216efa65
Un4givenPG <adam.adamczyk.sz@gmail.com>
2019-05-22 11:58:38 +0200

    Comments

--- includes/config.php
+++ includes/config.php
@@ -2,22 +2,22 @@
 ob_start();
 session_start();
 
-//set timezone
-date_default_timezone_set('Europe/London');
+//żeby dokumetacji stał się zadość
+date_default_timezone_set('Europe/Warsaw');
 
-//database credentials
+//tajne tajności tylko na serwerze broń Boże od githuba
 define('DBHOST','tajne_tajności');
 define('DBUSER','tajne_tajości');
 define('DBPASS','tajne_tajności');
 define('DBNAME','tajne_tajności');
 
-//application address
+
 define('DIR','http://gazeta-szczecinecka.pl/');
 define('SITEEMAIL','noreply@gazeta-szczecinecka.pl');
 
 try {
 
-	//create PDO connection
+	//Do MYSQL podłączenie
 	$db = new PDO("mysql:host=".DBHOST.";charset=utf8mb4;dbname=".DBNAME, DBUSER, DBPASS);
     //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -29,7 +29,7 @@ try {
     exit;
 }
 
-//include the user class, pass in the database connection
+//ta implmetacja php mailera za bardzo nie wyszła XD
 include('classes/user.php');
 include('classes/phpmailer/mail.php');
 $user = new User($db);
